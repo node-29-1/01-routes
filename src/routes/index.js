c2d10f4e2d440c43674ad594f5e4842cae7b87ff
Andres Mendoza <andres.david.mm@hotmail.com>
2023-10-24 22:52:19 -0500

    added projects list and members list

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const projects = [
+    {
+        "id": 1,
+        "name": "Pokedex",
+        "image": "https://res.cloudinary.com/ddflkwfsr/image/upload/v1682968805/102695607-39642200-4207-11eb-951b-390a856f9be4_u1qluv.png"
+    },
+    {
+        "id": 2,
+        "name": "Rick and morty project",
+        "image": "https://res.cloudinary.com/ddflkwfsr/image/upload/v1682968950/cufjzu9x8i7iuxmttzyf_jrnqna.png"
+    },
+    {
+        "id": 3,
+        "name": "Ecommerce",
+        "image": "https://res.cloudinary.com/ddflkwfsr/image/upload/v1682969085/1640011361-react-ecommerce-tutorial_aumyp3.png"
+    },
+    {
+        "id": 4,
+        "name": "Users crud",
+        "image": "https://res.cloudinary.com/ddflkwfsr/image/upload/v1682969207/fx2fni0aqudeaubuach9_bjgtbg.png"
+    }
+]
+
 // colocar las rutas aquí
 router.get('/', (req, res) => {
     res.render("home");
@@ -11,7 +34,27 @@ router.get('/about', (req, res) => {
 });
 
 router.get('/team', (req, res) => {
-    res.render("team");
+
+    const teamMembers = [
+        {
+            id: 1,
+            name: "Alexander"
+        },
+        {
+            id: 2,
+            name: "Bruno"
+        },
+        {
+            id: 3,
+            name: "Samuel",
+        },
+        {
+            id: 4,
+            name: "Victor",
+        }
+    ]
+
+    res.render("team", { teamMembers });
 });
 
 router.get('/team/:name', (req, res) => {
@@ -20,16 +63,17 @@ router.get('/team/:name', (req, res) => {
 });
 
 router.get('/about-me', (req, res) => {
-    res.send('sobre mi empresa');
+    res.render('about-me');
 });
 
 router.get('/projects', (req, res) => {
-    res.render('projects');
+    res.render('projects', { projects });
 });
 
 router.get('/projects/:id', (req, res) => {
     const { id } = req.params;
-    res.send(`Mostrando el proyecto: ${id}`);
+    const project = projects.find(project => project.id === +id);
+    res.render('project-detail', { id, project });
 })
 
 
